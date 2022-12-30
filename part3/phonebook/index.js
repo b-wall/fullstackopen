18c5fdbbffe0baf0aa67cdf61db8241285fa61cc
Ben W <b.wall.contact@gmail.com>
2022-12-30 16:02:10 +0000

    Complete exercises 3.13 and 3.14 - Connected app backend to MongoDB

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static('build'))
+app.use(express.static('build'));
+
+
+
+const Person = require('./models/person')
 
 let entries = [
     { 
@@ -30,7 +35,9 @@ let entries = [
 ]
 
 app.get('/api/persons', (request, response) => {
-    response.json(entries);
+  Person.find({}).then(people => {
+    response.json(people);
+  })
 });
 
 app.get('/info', (request, response) => {
@@ -41,14 +48,9 @@ app.get('/info', (request, response) => {
 });
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const entry = entries.find(entry => entry.id === id);
-
-    if(entry) {
-        response.json(entry);
-    } else {
-        response.status(404).end();
-    }
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
+  })
 });
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -60,33 +62,25 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons/', (request, response) => {
   const body = request.body;
-  if(!body.name || !body.number) {
+  if(body.name === undefined || body.number === undefined) {
     return response.status(400).json({
       error: 'content missing'
     });
   }
 
-  const existingName = entries.find(entry => entry.name.toLowerCase() === body.name.toLowerCase());
-  if(existingName) {
-    return response.status(400).json({
-      error: 'name already exists'
-    });
-  }
-
-  const id = Math.ceil(Math.random() * (100000 - 1) + 1);
-  const entry = {
-    "id": id,
+  const person = new Person({
     "name": body.name,
     "number": body.number
-  }
+  })
 
-  entries = entries.concat(entry);
-  response.json(entry);
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 });
 
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
