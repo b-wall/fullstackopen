05c2f65e249866372298cac42fd285dfaa7c9b58
Ben W <b.wall.contact@gmail.com>
2023-02-18 15:44:19 +0000

    Complete exercise 4.4 - further test functions

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const result = blogs.reduce((acc, blog) => acc += blog.likes || 0, 0)
+  return result
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
