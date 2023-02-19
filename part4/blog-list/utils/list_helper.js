8128bc9c38e865b3f8c64f0b4698ec7130b0eff3
Ben W <b.wall.contact@gmail.com>
2023-02-19 19:28:20 +0000

    Complete exercise 4.6 - Add mostBlogs function and test

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -16,14 +16,37 @@ const favouriteBlog = (blogs) => {
         likes: blog.likes
       }
     }
-    console.log(acc)
     return acc
   })
   return favourite
 }
 
+const mostBlogs = (blogs) => {
+  const authorPosts = blogs.reduce((acc, blog) => {
+    const key = blog.author
+    const value = acc[key] || 0 
+
+    return {...acc, [key] : value + 1}
+  }, {})
+  const formattedAuthors = Object.entries(authorPosts).map(([key, value]) => {
+    return {
+      author: key,
+      blogs: value
+    }
+  })
+  const topAuthor = formattedAuthors.reduce((acc, post) => post.blogs > acc.blogs ? post : acc)
+  console.log(topAuthor)
+  return topAuthor
+}
+
+// {
+//   author: "Robert C. Martin",
+//   blogs: 3
+// }
+
 module.exports = {
   dummy,
   totalLikes,
-  favouriteBlog
+  favouriteBlog,
+  mostBlogs
 }
