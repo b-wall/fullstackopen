de6830c414083a03a02e6f3fff256eebf9f0daa0
Ben W <b.wall.contact@gmail.com>
2023-02-25 17:42:47 +0000

    Complete exercise 4.15 - Create new users with post request

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,11 +1,11 @@
 const dummy = (blogs) => {
-  return 1
-}
+  return 1;
+};
 
 const totalLikes = (blogs) => {
-  const result = blogs.reduce((acc, blog) => acc += blog.likes || 0, 0)
-  return result
-}
+  const result = blogs.reduce((acc, blog) => (acc += blog.likes || 0), 0);
+  return result;
+};
 
 const favouriteBlog = (blogs) => {
   const favourite = blogs.reduce((acc, blog) => {
@@ -13,47 +13,58 @@ const favouriteBlog = (blogs) => {
       acc = {
         title: blog.title,
         author: blog.author,
-        likes: blog.likes
-      }
+        likes: blog.likes,
+      };
     }
-    return acc
-  })
-  return favourite
-}
+    return acc;
+  });
+  return favourite;
+};
 
 const mostBlogs = (blogs) => {
   const authorPosts = blogs.reduce((acc, blog) => {
-    const key = blog.author
-    const value = acc[key] || 0 
+    const key = blog.author;
+    const value = acc[key] || 0;
 
-    return {...acc, [key] : value + 1}
-  }, {})
+    return { ...acc, [key]: value + 1 };
+  }, {});
   const formattedAuthors = Object.entries(authorPosts).map(([key, value]) => {
     return {
       author: key,
-      blogs: value
-    }
-  })
-  const topAuthor = formattedAuthors.reduce((acc, post) => post.blogs > acc.blogs ? post : acc)
-  return topAuthor
-}
+      blogs: value,
+    };
+  });
+  const topAuthor = formattedAuthors.reduce((acc, post) =>
+    post.blogs > acc.blogs ? post : acc
+  );
+  return topAuthor;
+};
 
 const mostLikes = (blogs) => {
   const cleanedAuthors = blogs.reduce((acc, blog) => {
-    const key = blog.author
-    const value = acc[key] || 0
-    return {...acc, [key]: value + blog.likes}
-  }, {})
+    const key = blog.author;
+    const value = acc[key] || 0;
+    return { ...acc, [key]: value + blog.likes };
+  }, {});
 
-  const formattedAuthors = Object.entries(cleanedAuthors).map(([key, value]) => ({author: key, likes: value}))
-  .reduce((acc, author) => author.likes > acc.likes ? author : acc)
-  return formattedAuthors
-}
+  const formattedAuthors = Object.entries(cleanedAuthors)
+    .map(([key, value]) => ({ author: key, likes: value }))
+    .reduce((acc, author) => (author.likes > acc.likes ? author : acc));
+  return formattedAuthors;
+};
+
+const User = require("../models/user");
+
+const usersInDb = async () => {
+  const users = await User.find({});
+  return users.map((u) => u.toJSON());
+};
 
 module.exports = {
   dummy,
   totalLikes,
   favouriteBlog,
   mostBlogs,
-  mostLikes
-}
+  mostLikes,
+  usersInDb,
+};
